5b08f04ec846b8618105e1a909ccff8c47bfe70d
Público <pub@feelt1e30.ufu.br>
2022-11-03 08:38:09 -0300

    aritmInt OK

--- aritmInt.c
+++ aritmInt.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char** argv){
     int num1, num2;
-    scanf("Digite dois números: %d %d", &num1, &num2);
-    printf(" %d \n %d", num1, num2);
+    printf("Digite dois números: ");
+    scanf("%d %d", &num1, &num2);
     
     printf("%d + %d = %d  \n", num1, num2, num1+num2);
     printf("%d - %d = %d  \n", num1, num2, num1-num2);
